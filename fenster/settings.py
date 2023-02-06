42be750c38b7443106f628188c59f65ab065e606
Osca Mwongera <oscamwongera@gmail.com>
2023-02-06 15:21:39 +0100

    Load environment variables into project

--- fenster/settings.py
+++ fenster/settings.py
@@ -9,12 +9,16 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# python-dotenv
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -23,9 +27,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m8#@^p#ier9uvhp#73fk9uzv=r1q_=1^7-&btx&bzsy88(a$9='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+debug = os.getenv('DEBUG', False)
+DEBUG = debug
 
-ALLOWED_HOSTS = []
+hosts = os.getenv('ALLOWED_HOSTS', '0.0.0.0')
+
+ALLOWED_HOSTS = [hosts]
 
 
 # Application definition
